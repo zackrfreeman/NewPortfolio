fbbdb00825ffa30f6c41450e5a4055994fa769fb
Zack Freeman <zack4894@proton.me>
2024-11-23 22:15:50 -0500

    Initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,2 @@
+// This file is created automatically by Astro to generate Types for TypeScript purposes.
+/// <reference path="../.astro/types.d.ts" />
---
+++ src/js/nav.js
@@ -0,0 +1,24 @@
+const CSbody = document.querySelector("body");
+const CSnavbarMenu = document.getElementById("cs-navigation");
+const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+
+function toggleMenu() {
+  mobileMenuToggle.classList.toggle("cs-active");
+  CSnavbarMenu.classList.toggle("cs-active");
+  CSbody.classList.toggle("cs-open");
+}
+
+// Checks the value of aria expanded on an element and changes it accordingly whether it is expanded or not
+function ariaExpanded(element) {
+  const isExpanded = element.getAttribute("aria-expanded");
+  element.setAttribute(
+    "aria-expanded",
+    isExpanded === "false" ? "true" : "false",
+  );
+}
+
+// Toggles the hamburger mobile menu
+mobileMenuToggle.addEventListener("click", function () {
+  toggleMenu();
+  ariaExpanded(mobileMenuToggle);
+});
